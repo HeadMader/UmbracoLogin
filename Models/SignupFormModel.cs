4759b05cbc9bc64a9068caa7a2e84c0f05cfe2c5
Headmader <vladnagornyy2030@gmail.com>
2024-08-25 20:14:58 +0300

    Add required keyword to SignupFormModel properties

--- Models/SignupFormModel.cs
+++ Models/SignupFormModel.cs
@@ -6,22 +6,22 @@ namespace UmbracoLogin.Models
     {
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
         [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         [Required(ErrorMessage = "First Name is required.")]
-        public string FirstName { get; set; }
+        public required string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required.")]
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
         [Required(ErrorMessage = "Mobile number is required.")]
         [RegularExpression(@"^\s*(?:\+?(\d{1,3}))?[- (]*(\d{3})[- )]*(\d{3})[- ]*(\d{4})(?: *[x/#]{1}(\d+))?\s*$", ErrorMessage = "Invalid mobile number.")]
         [Phone(ErrorMessage = "Invalid mobile number.")]
-        public string Mobile { get; set; }
+        public required string Mobile { get; set; }
 
         [Required(ErrorMessage = "Country is required.")]
         public int CountryID { get; set; }
