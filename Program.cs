5a7868d8193da86737a570d46a5fb70ee0f25bb1
Headmader <vladnagornyy2030@gmail.com>
2024-08-25 20:17:11 +0300

    Remove response compression from ASP.NET Core app

--- Program.cs
+++ Program.cs
@@ -1,29 +1,8 @@
-using Microsoft.AspNetCore.ResponseCompression;
-using System.Data.Common;
-using System.IO.Compression;
 using Umbraco.Cms.Persistence.Sqlite;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddWebOptimizer(pipeline => pipeline.MinifyCssFiles("css/**/*.css"));
-
-builder.Services.AddResponseCompression(options =>
-{
-	options.EnableForHttps = true;
-	options.Providers.Add<BrotliCompressionProvider>();
-	options.Providers.Add<GzipCompressionProvider>();
-});
-
-builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
-{
-	options.Level = CompressionLevel.Fastest;
-});
-
-builder.Services.Configure<GzipCompressionProviderOptions>(options =>
-{
-	options.Level = CompressionLevel.SmallestSize;
-});
-
 
 builder.CreateUmbracoBuilder()
 	.AddUmbracoSqliteSupport()
@@ -36,8 +15,6 @@ builder.CreateUmbracoBuilder()
 WebApplication app = builder.Build();
 
 await app.BootUmbracoAsync();
-
-app.UseResponseCompression();
 
 app.UseWebOptimizer();
 app.UseUmbraco()
